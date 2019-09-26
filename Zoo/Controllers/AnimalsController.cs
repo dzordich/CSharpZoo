095952be3dbf401876365fa61fa2dc3f1b5a5f16
dzordich <dzordich@gmail.com>
2019-09-26 13:23:26 -0400

    cleaned up controllers

--- Zoo/Controllers/AnimalsController.cs
+++ Zoo/Controllers/AnimalsController.cs
@@ -31,52 +31,15 @@ namespace Zoo.Controllers
 
         }
 
-        [HttpGet("{name}/speak")]
-        public async Task<IActionResult> AnimalSpeak(string name)
-        {
-            Factory animalspeaker = new Factory();
-            IAnimal animal = animalspeaker.CreateAnimal(name);
-            return Created("AnimalSpeak", animal.Speak());
-        }
-
-
-
-        // GET: api/Animals
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<IAnimal>>> GetAnimals()
+        //[HttpGet("{name}/speak")]
+        //public async Task<IActionResult> AnimalSpeak(string name)
         //{
-        //    return await _context.Animals.ToListAsync();
+        //    Factory animalspeaker = new Factory();
+        //    IAnimal animal = animalspeaker.CreateAnimal(name);
+        //    return Created("AnimalSpeak", animal.Speak());
         //}
 
-        //// GET: api/Animals/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<IAnimal>> GetAnimal(long id)
-        //{
-        //    var animal = await _context.Animals.FindAsync(id);
 
-        //    if (animal == null)
-        //    {
-        //        return NotFound();
-        //    }
 
-        //    return animal;
-        //}
-
-        //// POST: api/Animals
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        //// more details see https://aka.ms/RazorPagesCRUD.
-        //[HttpPost]
-        //public async Task<ActionResult<Animal>> PostAnimal(Animal animal)
-        //{
-        //    _context.Animals.Add(animal);
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction(nameof(GetAnimal), new { id = animal.Id }, animal);
-        //}
-
-        //private bool AnimalExists(long id)
-        //{
-        //    return _context.Animals.Any(e => e.Id == id);
-        //}
     }
 }
